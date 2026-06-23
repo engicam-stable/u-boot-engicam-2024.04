88c512f21e8adc4fd7cd79141038ffc630531f23
engicam <support@engicam.com>
2026-06-23 16:03:54 +0200

    IMX91Microgea: EVB support, rev 0651A support

--- board/engicam/imx91_microgea/spl.c
+++ board/engicam/imx91_microgea/spl.c
@@ -32,7 +32,7 @@
 #include <asm/arch/ccm_regs.h>
 #include <asm/arch/ddr.h>
 #include <power/pmic.h>
-#include <power/pca9450.h>
+#include <power/pf9453.h>
 #include <asm/arch/trdc.h>
 
 DECLARE_GLOBAL_DATA_PTR;
@@ -74,61 +74,51 @@ void spl_dram_init(void)
 	ddr_init(ptiming);
 }
 
-#if CONFIG_IS_ENABLED(DM_PMIC_PCA9450)
+#if CONFIG_IS_ENABLED(DM_PMIC_PF9453)
 int power_init_board(void)
 {
 	struct udevice *dev;
 	int ret;
-	unsigned int val = 0, buck_val;
+	unsigned int buck_val;
 
-	ret = pmic_get("pmic@25", &dev);
-	if (ret != 0) {
-		puts("ERROR: Get PMIC PCA9451A failed!\n");
-		return ret;
+	ret = pmic_get("pmic@32", &dev);
+	if (ret == -ENODEV) {
+		puts("No pf9453@32\n");
+		return 0;
 	}
-	puts("PMIC: PCA9451A\n");
-	/* BUCKxOUT_DVS0/1 control BUCK123 output */
-	pmic_reg_write(dev, PCA9450_BUCK123_DVS, 0x29);
+	if (ret != 0)
+		return ret;
 
 	/* enable DVS control through PMIC_STBY_REQ */
-	pmic_reg_write(dev, PCA9450_BUCK1CTRL, 0x59);
-
-	ret = pmic_reg_read(dev, PCA9450_PWR_CTRL);
-	if (ret < 0)
-		return ret;
-	else
-		val = ret;
+	pmic_reg_write(dev, PF9453_BUCK2CTRL, 0x59);
 
 	if (is_voltage_mode(VOLT_LOW_DRIVE)) {
-		buck_val = 0x0c; /* 0.8v for Low drive mode */
+		buck_val = 0x10; /* 0.8v for Low drive mode */
 		printf("PMIC: Low Drive Voltage Mode\n");
 	} else if (is_voltage_mode(VOLT_NOMINAL_DRIVE)) {
-		buck_val = 0x10; /* 0.85v for Nominal drive mode */
+		buck_val = 0x14; /* 0.85v for Nominal drive mode */
 		printf("PMIC: Nominal Voltage Mode\n");
 	} else {
-		buck_val = 0x14; /* 0.9v for Over drive mode */
+		buck_val = 0x18; /* 0.9v for Over drive mode */
 		printf("PMIC: Over Drive Voltage Mode\n");
 	}
 
-	if (val & PCA9450_REG_PWRCTRL_TOFF_DEB) {
-		pmic_reg_write(dev, PCA9450_BUCK1OUT_DVS0, buck_val);
-		pmic_reg_write(dev, PCA9450_BUCK3OUT_DVS0, buck_val);
-	} else {
-		pmic_reg_write(dev, PCA9450_BUCK1OUT_DVS0, buck_val + 0x4);
-		pmic_reg_write(dev, PCA9450_BUCK3OUT_DVS0, buck_val + 0x4);
-	}
-
-	/* Set VDDQ to 1.1V from buck2 (buck2 not used for iMX91 EVK) */
-	pmic_reg_write(dev, PCA9450_BUCK2OUT_DVS0, 0x28);
+	pmic_reg_write(dev, PF9453_BUCK2OUT, buck_val);
 
 	/* set standby voltage to 0.65v */
-	if (val & PCA9450_REG_PWRCTRL_TOFF_DEB)
-		pmic_reg_write(dev, PCA9450_BUCK1OUT_DVS1, 0x0);
-	else
-		pmic_reg_write(dev, PCA9450_BUCK1OUT_DVS1, 0x4);
+	pmic_reg_write(dev, PF9453_BUCK2OUT_STBY, 0x4);
 
-	/* I2C_LT_EN*/
-	pmic_reg_write(dev, 0xa, 0x3);
+	/* 1.1v for LPDDR4 */
+	pmic_reg_write(dev, PF9453_BUCK1OUT, 0x14);
+
+	ret = pmic_reg_read(dev, PF9453_CONFIG1);
+	if (ret < 0)
+		return ret;
+	/*The timer default is 8sec and too long, so change to 100ms.*/
+	pmic_reg_write(dev, PF9453_CONFIG1, ~PF9453_RESETKEY_TIMER_MASK & ret);
+
+	/* set WDOG_B_CFG to cold reset */
+	pmic_reg_write(dev, PF9453_RESET_CTRL, 0xA0);
 	return 0;
 }
 #endif
